547a0e4613d65e1980c36c708cec5135f0911b66
tong.yao <tong.yao@check24.de>
2023-03-24 12:50:02 +0100

    Test the button color and clickEvent

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('correct initial color, and updates when clicked', () => {
   render(<App />);
-  const linkElement = screen.getByRole('link', { name: /learn react/i });
-  expect(linkElement).toBeInTheDocument();
+  // find the element by the role of button and initial text
+  const colorButton = screen.getByRole('button', {
+    name: 'Change to blue',
+  });
+
+  //expect the background color is red
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+
+  //click Button
+  fireEvent.click(colorButton);
+
+  //expect the backgournd color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+
+  // expect the button text to be 'change to red'
+  expect(colorButton).toHaveTextContent('Change to red');
 });
